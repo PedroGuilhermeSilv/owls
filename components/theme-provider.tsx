54162ca15bc0ef7943e6a0a13ea6a7e771a29cf7
guilherme <vinespedro55@gmail.com>
2025-03-15 15:10:41 -0300

    fix: them-provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,19 @@
 "use client"
 
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
+import * as React from "react"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+    const [mounted, setMounted] = React.useState(false)
+
+    // Only render children when mounted on client
+    React.useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return <>{children}</>
+    }
+
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 } 
